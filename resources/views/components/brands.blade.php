e90d9ed688ab31f580eb0e8713de97a533f2b5cf
Caxivitual <destinyhero789@gmail.com>
2025-09-27 21:19:57 +0700

    remove brands component

--- resources/views/components/brands.blade.php
+++
@@ -1,8 +0,0 @@
-<div class="single-widget category">
-    <h3 class="title">Hãng</h3>
-    <ul class="category-list">
-        @foreach($brands as $brand)
-            <li><a href="{{route('product-brand',$brand->slug)}}">{{$brand->title}}</a></li>
-        @endforeach
-    </ul>
-</div>
