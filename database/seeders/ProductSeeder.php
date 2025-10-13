193520f04733aaec83622f2c72f5f69c9c9c71cf
Feliss <thanhlta1234@gmail.com>
2025-10-13 21:32:59 +0700

    seeder

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -19,8 +19,8 @@ class ProductSeeder extends Seeder
         $productImages = Storage::disk('public')->files('photos/1/Products');
 
         $categorySets = [
-            1 => [ 5, 6, 7, 8 ],
-            2 => [ 9, 10 ],
+            1 => [5, 6, 7, 8],
+            2 => [9, 10],
             3 => null,
             4 => null,
         ];
@@ -32,12 +32,57 @@ class ProductSeeder extends Seeder
             $childCategoryId = $categorySets[$categoryId] == null ? null : $categorySets[$categoryId][array_rand($categorySets[$categoryId])];
 
             return [
-                'title' => pathinfo($image, PATHINFO_FILENAME),
+                'title' => $this->humanizeFilename($image),
                 'photo' => '/storage/' . $image,
                 'cat_id' => $categoryId,
                 'child_cat_id' => $childCategoryId,
                 'brand_id' => Brand::inRandomOrder()->take(1)->get()[0]->id,
             ];
         })->create();
+    }
+
+    private function humanizeFilename(string $path): string
+    {
+        // Lấy filename không chứa extension
+        $name = pathinfo($path, PATHINFO_FILENAME);
+
+        // Thay dấu gạch dưới/gạch ngang thành dấu cách
+        $name = preg_replace('/[_-]+/', ' ', $name);
+
+        // Xóa tiền tố số nếu có (ví dụ 1612870846-...)
+        $name = preg_replace('/^[0-9]+\s*/', '', $name);
+
+        // mapping từ token ascii -> từ có dấu/chuẩn tiếng Việt
+        $map = [
+            'dong' => 'đồng',
+            'ho' => 'hồ',
+            'bao' => 'báo',
+            'thuc' => 'thức',
+            'co' => 'cổ',
+            'con' => 'con',
+            'lac' => 'lắc',
+            'de' => 'để',
+            'ban' => 'bàn',
+            'dien' => 'điện',
+            'tu' => 'tử',
+            'la' => 'La',
+            'ma' => 'mã',
+            'rolex' => 'Rolex',
+            'so' => 'số',
+            'treo' => 'treo',
+            'tuong' => 'tường',
+            // Thêm mapping khác nếu cần
+        ];
+
+        $words = preg_split('/\s+/', mb_strtolower(trim($name), 'UTF-8'));
+
+        $mapped = array_map(function ($w) use ($map) {
+            return $map[$w] ?? $w;
+        }, $words);
+
+        // Viết hoa mỗi từ theo chuẩn Unicode
+        $title = mb_convert_case(implode(' ', $mapped), MB_CASE_TITLE, 'UTF-8');
+
+        return $title;
     }
 }
